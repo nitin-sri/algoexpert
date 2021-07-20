8aaa13f866adf016fce131cb94d0d1502921b04f
nitin-sri <srivastava.nitin@gmail.com>
2021-07-20 09:06:27 +0100

    declare methods

---
+++ src/main/java/SubstringUnderscorifier.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class SubstringUnderscorifier {
+    public String underscorifySubstring(String str, String substring){
+        return null;
+    }
+
+    public List<Integer[]> getLocations(String str, String substring){
+        return null;
+    }
+
+    public List<Integer[]> collapse(List<Integer[]> locations){
+        return null;
+    }
+
+    public String underscorify(String str, List<Integer[]> locations ){
+        return null;
+    }
+}
